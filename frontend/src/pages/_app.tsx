bc9e61543ab43dcea6d56b8a3870af3a4bc9bacb
Josh DeCristi <jdecristi777a@gmail.com>
2023-03-13 17:05:03 -0400

    Configured frontend with bootstrap

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -1,6 +1,21 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "@/styles/theme.scss";
+
+import { Lexend } from "next/font/google";
+import SSRProvider from "react-bootstrap/SSRProvider";
+
+import type { AppProps } from "next/app";
+
+const lexend = Lexend({
+  subsets: ["latin"],
+  weight: ["500", "900"],
+});
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <SSRProvider>
+      <main className={lexend.className}>
+        <Component {...pageProps} />
+      </main>
+    </SSRProvider>
+  );
 }
--- frontend/src/pages/_document.tsx
+++ frontend/src/pages/_document.tsx
@@ -1,13 +1,15 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <link rel="icon" type="image/svg" sizes="16x16" href="/Favicon.svg" />
+      </Head>
       <body>
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
